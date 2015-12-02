84da43665f3fc307536e7014abab7bc92ae5283e
iquintasgonzalez <iquintasgonzalez@atanasoft28.danielcastelao.org>
2015-12-02 10:58:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/yperezmartinez/rssreader/DetailFragment.java
@@ -0,0 +1,27 @@
+package com.example.yperezmartinez.rssreader;
+
+/**
+ * Created by yperezmartinez on 11/18/15.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_rssitem_detail,
+                container, false);
+        return view;
+    }
+
+    public void setText(String url) {
+        TextView view = (TextView) getView().findViewById(R.id.detailsText);
+        view.setText(url);
+    }
+}
---
+++ app/src/main/java/com/example/yperezmartinez/rssreader/RssfeedActivity.java
@@ -0,0 +1,24 @@
+package com.example.yperezmartinez.rssreader;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class RssfeedActivity extends Activity implements MyListFragment.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rssfeed);
+    }
+
+    @Override
+    public void onRssItemSelected(String link) {
+        DetailFragment fragment = (DetailFragment) getFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+        fragment.setText(link);
+    }
+
+}
+
+
